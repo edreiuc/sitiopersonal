37fcd5030ca2d70b366f9b08514937787472cf7e
samuel <samuel@rcsistema.com>
2015-02-11 00:21:50 -0600

    maps

--- contact.php
+++ contact.php
@@ -27,8 +27,8 @@ include 'header.php';
 							</form>
 					</div>
 				</div>
-				<div class="col-md-6" style="padding-top: 6%">
-					<div id="map-canvas"></div>
+				<div class="col-md-6" style="padding-top: 2%">
+					<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3726.839400813739!2d-89.708234!3d20.91878!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8f560d4f16d8e475%3A0x82c65d74f808a977!2s17-D+394%2C+Acim%2C+97390+M%C3%A9rida%2C+Yuc.!5e0!3m2!1ses-419!2smx!4v1423605403091" width="100%" height="500" frameborder="0" style="border:0"></iframe>
 				</div>
 			</div>
 		</div>
