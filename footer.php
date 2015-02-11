4aeceeed030d2aed3ea499e3a627efa2a584c768
samuel <samuel@rcsistema.com>
2015-02-11 10:31:18 -0600

    merge solved

--- footer.php
+++ footer.php
@@ -19,7 +19,7 @@
 			</div>
 		</div>
 	<script type="text/javascript" src="js/jquery.js"></script>
-	<script type="text/javascript" src="bootstrap/js/bootstrap.min.js"></script>		
+	<script type="text/javascript" src="bootstrap/js/bootstrap.min.js"></script>
 </body>
 
 
