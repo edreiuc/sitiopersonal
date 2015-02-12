49b333f282d9f1d04b08a869907c8464ba5cd454
edreiuc <sam_edreiuc@live.com>
2015-02-12 03:14:47 -0600

    redimention to ipad2mini

--- contact.php
+++ contact.php
@@ -7,7 +7,7 @@ include 'header.php';
 				<div class="col-md-6" style="padding-top: 2%">
 					<div class="jumbotron">
 						<h2>Contactame</h2>
-							<form method="POST" name="fcontacto" id="fcontacto" action="./">
+							<form method="POST" name="fcontacto" id="fcontacto" action="send.php">
 								<div class="row">
 									<div class="col-md-6">
 										<label>Nombre:</label>
@@ -28,7 +28,7 @@ include 'header.php';
 					</div>
 				</div>
 				<div class="col-md-6" style="padding-top: 2%">
-					<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3726.839400813739!2d-89.708234!3d20.91878!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8f560d4f16d8e475%3A0x82c65d74f808a977!2s17-D+394%2C+Acim%2C+97390+M%C3%A9rida%2C+Yuc.!5e0!3m2!1ses-419!2smx!4v1423605403091" width="100%" height="500" frameborder="0" style="border:0"></iframe>
+					<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3726.8394555122004!2d-89.70821749999999!3d20.918777800000004!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8f560d4f6b757463%3A0x21f8c274ed8d2578!2s17-D+391%2C+Acim%2C+97390+M%C3%A9rida%2C+Yuc.!5e0!3m2!1sfr!2smx!4v1423687312492" width="100%" height="500" frameborder="0" style="border:0"></iframe>
 				</div>
 			</div>
 		</div>
